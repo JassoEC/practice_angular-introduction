d053278b73037e94328e89e00124dc7b1db6a225
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-08-17 10:08:50 -0500

    se agrego el ejemplo de constructores y herencia de classes

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,22 @@
-class Hero {
-  name: string;
-  age: number;
-  realName: string;
-
-  constructor(name: string, age?: number, realname = "") {
-    this.name = name;
-    this.age = age;
-    this.realName = realname;
+class NormalPeople {
+  constructor(public name: string, public address: string) {}
+}
+class Hero extends NormalPeople {
+  constructor(
+    public alterEgo: string,
+    public age: number,
+    public realName: string
+  ) {
+    super(realName, "");
   }
 }
-
-/* cuando no se define de forma explicita un modificador de
-  acceeso de las propiedades de una clase por defiinicion 
-  todas son publicas. */
 
 class Hero2 {
   constructor(public name: string, public age: number) {}
 }
 
+const mane = new NormalPeople("Emanuel", "Chapala");
 const ironMan = new Hero("Iron Man", 40, "Anthony Stark");
+
+console.log(mane);
 console.log(ironMan.name);
-
-const captain = new Hero2("Captain America", 80);
-
-console.log(captain.name);
